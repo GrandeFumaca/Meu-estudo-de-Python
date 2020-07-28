013c09b17338e50a43dc7c80d4e036673586ae1a
Bück Dich <68882814+GrandeFumaca@users.noreply.github.com>
2020-07-27 23:56:21 -0300

    Create Salario.py

---
+++ Salario.py
@@ -0,0 +1,3 @@
+salario=float(input('Qual seu salário atual? R$'))
+calculo=(salario*115)/100
+print(f'Seu salário era de R${salario}, seu reajuste foi de 15%, agora passando a ganhar R${calculo:.2f}.')
