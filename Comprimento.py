2eadb928a9d5f8f7c2acda6abceb3018767b408d
Bück Dich <68882814+GrandeFumaca@users.noreply.github.com>
2020-07-27 23:48:58 -0300

    Create Comprimento.py

---
+++ Comprimento.py
@@ -0,0 +1,8 @@
+defender=int(input('Digite um valor em metros: '))
+b=defender/10
+c=defender/100
+d=defender/1000
+e=defender*10
+f=defender*100
+g=defender*1000
+print(f'A medida é {defender}m, nos seguintes valores são: \n{b}dam\n{c}hm\n{d}km\n{e}dm\n{f}cm\n{g}mm')
