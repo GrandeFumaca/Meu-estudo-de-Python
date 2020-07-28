a4bcb15cebcb66921fc42d40636dcadb731d3042
Bück Dich <68882814+GrandeFumaca@users.noreply.github.com>
2020-07-28 08:59:56 -0300

    Create Robóticos.py

---
+++ Robóticos.py
@@ -0,0 +1,7 @@
+from random import randint
+bonk=randint(0,5)
+mangalho=int(input('Pensei em um número de 0 a 5, qual número eu pensei? '))
+if mangalho==bonk:
+    print('Puta vida! Os humanos vão comer meu cu!')
+else:
+    print('HAHAHAHAHAHHAAHAHAHA! EU VOU COMER O CU DOS HUMANOS!')
