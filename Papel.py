89977454c3917e6851880394fa161eacfb60e693
Bück Dich <68882814+GrandeFumaca@users.noreply.github.com>
2020-07-27 23:48:02 -0300

    Create Papel.py

---
+++ Papel.py
@@ -0,0 +1,4 @@
+escrevi_e=float(input('Digite a primeira nota do aluno: '))
+sai_correndo=float(input('Digite a segunda nota do aluno: '))
+de_quem_tá_lendo=(sai_correndo+escrevi_e)/2
+print(f'A nota final do aluno é {de_quem_tá_lendo:.1f}')
