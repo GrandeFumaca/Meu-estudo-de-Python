fe05ab0d88430519fe6298dc1b38c55ad8920e82
Bück Dich <68882814+GrandeFumaca@users.noreply.github.com>
2020-07-28 08:29:40 -0300

    Create documentario vermelhopilado.py

---
+++ documentario vermelhopilado.py
@@ -0,0 +1,6 @@
+import math
+BIG_RED=float(input('Digite um número para encontrar o seno, cosseno e tangente, que será arredondada para cima: '))
+i_reading_fuckface=math.sin(math.radians(BIG_RED))
+patriacardo=math.cos(math.radians(BIG_RED))
+SHUT_A_FUCK_UP=math.tan(math.radians(BIG_RED))
+print(f'O seno de {BIG_RED} é {i_reading_fuckface:.2f}.\nO cosseno de {BIG_RED} é {patriacardo:.2f}\nO tangente de {BIG_RED} é {SHUT_A_FUCK_UP:.2f}.')
