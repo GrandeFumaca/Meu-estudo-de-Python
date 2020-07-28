deda6d87c8cc8b2aefae6ee34819378c2554442f
Bück Dich <68882814+GrandeFumaca@users.noreply.github.com>
2020-07-27 23:50:57 -0300

    Create Mina.py

---
+++ Mina.py
@@ -0,0 +1,13 @@
+uniao_flasco=int(input('Digite um número: '))
+mano_sem_cao=uniao_flasco*0
+batendo_punheta=uniao_flasco*1
+com_o_pau=uniao_flasco*2
+no_ventilador=uniao_flasco*3
+se_essa=uniao_flasco*4
+mina_falar=uniao_flasco*5
+merda_eu=uniao_flasco*6
+vou_cortar=uniao_flasco*7
+o_cabelo=uniao_flasco*8
+com_o_barbeador=uniao_flasco*9
+cu=uniao_flasco*10
+print(f'A seguir surgirá a tabuada do número {uniao_flasco}.\n{uniao_flasco} x 0 = {mano_sem_cao}\n{uniao_flasco} x 1 = {batendo_punheta}\n{uniao_flasco} x 2 = {com_o_pau}\n{uniao_flasco} x 3 = {no_ventilador}\n{uniao_flasco} x 4 = {se_essa}\n{uniao_flasco} x 5 = {mina_falar}\n{uniao_flasco} x 6 = {merda_eu}\n{uniao_flasco} x 7 = {vou_cortar}\n{uniao_flasco} x 8 = {o_cabelo}\n{uniao_flasco} x 9 = {com_o_barbeador}\n{uniao_flasco} x 10 = {cu}')
