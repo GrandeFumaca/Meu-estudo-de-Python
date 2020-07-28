85a6bec36800736b038716536336bd25e801ebb5
Bück Dich <68882814+GrandeFumaca@users.noreply.github.com>
2020-07-28 08:59:14 -0300

    Create Nomesão.py

---
+++ Nomesão.py
@@ -0,0 +1,4 @@
+nome=str(input('Digite seu nome: ')).strip()
+cu_um=nome.split()[0]
+cu_dois=nome.split()[-1]
+print(f'Muito prazer em te conhecer seu bunda-mole!\nSei com meu voodoo que seu primeiro nome é {cu_um}\nSeu segundo nome é {cu_dois}, pois eu vi no seu documento que roubei, trouxa')
