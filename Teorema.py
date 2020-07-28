6c813ad5a4d1fa14952a4ec7a029510873ca46aa
Bück Dich <68882814+GrandeFumaca@users.noreply.github.com>
2020-07-28 08:27:33 -0300

    Create Teorema.py

---
+++ Teorema.py
@@ -0,0 +1,4 @@
+co=int(input('Digite o tamanho do cateto oposto: '))
+ca=int(input('Digite o tamanho do cateto adjacente: '))
+hipotenusa=co**2+ca**2
+print(f'A soma do cateto oposto e do cateto adjacente é {hipotenusa}')
