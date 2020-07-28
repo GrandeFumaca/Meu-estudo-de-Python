79a2149ac714fc3add2e7b949fad796c446b3db3
Bück Dich <68882814+GrandeFumaca@users.noreply.github.com>
2020-07-27 23:44:35 -0300

    Create Maicon Juegueson.py

---
+++ Maicon Juegueson.py
@@ -0,0 +1,4 @@
+micheal_jackson=int(input('Escreva um número: '))
+está=micheal_jackson+1
+vivo_em_macapá=micheal_jackson-1
+print('O sucessor de {} é {} e o antecessor é {}.'.format(micheal_jackson, está, vivo_em_macapá))
