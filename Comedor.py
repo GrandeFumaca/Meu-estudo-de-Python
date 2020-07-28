bfee6403cd2ecbaa5ff24944e3ae32e4638328aa
Bück Dich <68882814+GrandeFumaca@users.noreply.github.com>
2020-07-27 23:45:57 -0300

    Create Comedor.py

---
+++ Comedor.py
@@ -0,0 +1,5 @@
+eu=int(input('Digite um número: '))
+como=eu*2
+casadas=eu*3
+de_calcinha=eu**1/2
+print('O duplo de {} é {}, o triplo é {} e a raíz quadrada é {:.2f}.'.format(eu, como, casadas, de_calcinha))
