a4a544695277a48483927aec55e9c7adfc4690a0
Bück Dich <68882814+GrandeFumaca@users.noreply.github.com>
2020-07-28 08:33:19 -0300

    Create Choice.py

---
+++ Choice.py
@@ -0,0 +1,8 @@
+from random import choice
+cu=str(input('Digite o nome de um aluno: '))
+vagina=str(input('Digite o nome de um aluno: '))
+tetas=str(input('Digite o nome de um aluno: '))
+escroto=str(input('Digite o nome de um aluno: '))
+morte=[cu, vagina, tetas, escroto]
+sorte=choice(uniao_flasco)
+print(f'O aluno escolhido para o surubão é {sorte}')
