bc0bf780837d52775c0c7476ec9cd5285aa930d0
Bück Dich <68882814+GrandeFumaca@users.noreply.github.com>
2020-07-28 08:56:36 -0300

    Create UDCM.py

---
+++ UDCM.py
@@ -0,0 +1,3 @@
+numeração=int(input('Digite uma numeração: '))
+calculo=f'{numeração:>4}'
+print(f'Eae meu mano, a numeração ficou da seguinte forma:\nU n i d a d e = {calculo[0]}\nD e z e n a = {calculo[1]}\nC e n t e n a = {calculo[2]}\nM i l h a r = {calculo[3]}')
